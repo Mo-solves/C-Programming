911efd997d1735bd3a53d5b9d44b64c8a8a19cdb
Mohamed Abdi <mosolves147@gmail.com>
2022-03-26 17:08:05 +0300

    First cpp program print Hello world

---
+++ 1.Intro-cpp/1.first-cpp.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "My name is Mohamed" << std::endl;
+    std::cout << "This is my First Time Learning in c++" << std::endl;
+    return 0;
+}
