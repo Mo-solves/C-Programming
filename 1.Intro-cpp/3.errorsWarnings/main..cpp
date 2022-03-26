d92189603432e691c995d5aec4b74b8b333cca65
Mohamed Abdi <mosolves147@gmail.com>
2022-03-26 17:58:42 +0300

    Error and Warnings in c++

---
+++ 1.Intro-cpp/3.errorsWarnings/main..cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    // Compile time error
+    // std::cout << "Compile time error" << std::endl
+    std::cout << "Compile time error" << std::endl;
+
+    // Run time error
+    // 7 / 0;
+    std::cout << "Run time Error" << std::endl;
+    return 0;
+}
